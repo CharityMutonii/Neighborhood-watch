a01788b09df1f5615717b1547e1e39dc4ef70d99
Charity Mutoni <charitytoni0@gmail.com>
2021-05-06 21:01:05 +0200

    added models to our Admin page

--- hoods/admin.py
+++ hoods/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Neighborhood,Profile,Business,Alert
 
-# Register your models here.
+admin.site.register(Neighborhood)
+admin.site.register(Alert)
+admin.site.register(Profile)
+admin.site.register(Business)
+
+
