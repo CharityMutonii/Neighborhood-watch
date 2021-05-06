f2d39c9142244fea88b467c39ee5f8a1f399b6dc
Charity Mutoni <charitytoni0@gmail.com>
2021-05-06 20:17:24 +0200

    created navbar

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url,include
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'',include('hoods.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.LoginView.as_view(template_name='registration/login.html'), {"next_page": '/'}),
+    url(r'^logout/$', views.LogoutView.as_view(template_name='registration/logout.html'), {"next_page": '/'}),
 ]
