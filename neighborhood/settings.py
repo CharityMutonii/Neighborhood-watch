d062186833efe259ba4e1ee6c3f835d4dba3858b
Charity Mutoni <charitytoni0@gmail.com>
2021-05-06 19:54:38 +0200

    changed my projects database settings

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -75,11 +75,12 @@ WSGI_APPLICATION = 'neighborhood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hoods',
+        'USER': 'charity',
+    'PASSWORD':'21193',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
